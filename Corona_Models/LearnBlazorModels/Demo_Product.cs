1c6abc5a65607aa5be8ccc8ef2c8c4b9b93479af
porslin <haashira@icloud.com>
2023-08-23 01:33:34 +0200

    connecting model to view and properties to objects and using bind value

---
+++ Corona_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// creating first model as demo_product
+
+namespace Corona_Models.LearnBlazorModels
+{
+    // 1. first change this class to public
+    public class Demo_Product
+    {
+        // 2. create three basic properties of the product you will work with
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsActive { get; set; }
+
+        //adding a new property. rmbr properties must start with Caps
+        public double Price { get; set; }
+    }
+}
+
+// 3. time to use this model in the bindprop component under the blazor server project
+
