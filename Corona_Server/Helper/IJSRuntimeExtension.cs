007e20296b124867a13c2911c2fa466842f87d16
porslin <haashira@icloud.com>
2023-08-26 23:06:05 +0200

    toastrjs and jsruntime extensions

---
+++ Corona_Server/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.JSInterop;
+
+namespace Corona_Server.Helper
+{
+    // this class will be static so theres no need to create objects. 
+    // so all the extension methods will also be static.
+    public static class IJSRuntimeExtension
+    {
+        // first method for toastrsuccess. and then on what this will be extending, so itll be this IJSRuntime with the name jsRuntime
+        // then pass the parameters, in this case 
+        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            // moving this from blazorjs to here.
+            // removing underscore on _JSRuntime
+            // matching jsRuntime name above to this
+            // parameters: method, type, message -> in this order
+            // the success here is matching the type in commonjs, so everything in lowercase.
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        
+        // method for error
+        public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+
+        // with that i have added two extension methods to the runtime
+    }
+}
---
+++ Corona_Server/wwwroot/js/common.js
@@ -0,0 +1,12 @@
+﻿// creating a showtoastr and using window. to make it globally available
+// passing 2 parameters: type of notification and the message
+window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        // using the default options from the toastr guide
+        toastr.success(message, "Operation Successful", { timeOut: 5000 });
+    }
+    if (type === "error") {
+        // using the default options from the toastr guide
+        toastr.error(message, "Operation Failed", { timeOut: 5000 });
+    }
+}
