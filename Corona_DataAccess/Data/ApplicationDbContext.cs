9c665ef9609de616ae310dffa0843a0e2dcddee5
porslin <haashira@icloud.com>
2023-08-27 23:17:29 +0200

    ApplicationDbContext setup

---
+++ Corona_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corona_DataAccess.Data
+{
+    // in order to use EF Core, the EF must use the DbContext. Change class to public. 
+    public class ApplicationDbContext : DbContext
+    {
+        // to configure EF Core, create a ctor inside the ApplicationDbContext. Within the ctor receive the DbContextOptions. This options will be on the class ApplicationDbContext. Then pass them to the base class of DbContext.
+        // so wtv options which has a connectionstring that was received when this class was instantiated, that needs to be passed to DbContext. 
+        // this is the default config that is needed with DbContext.
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        // now, create a category table in the db. So write DbSet of class CAtegory. and name it categories. this will create a table with the name of categories. 
+        public DbSet<Category> Categories { get; set; }
+        
+
+        // to create a table, you need to create a SQL server adn establish a connection
+    }
+}
---
+++ Corona_DataAccess/Data/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// there will be multiple products in this website. and all of products will belong to some category. 
+
+// remove .Date from the category namespace
+namespace Corona_DataAccess
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        // here createdDate is used for logging purposes, and not in the web app. 
+        public DateTime CreatedDate { get; set; }
+    }
+}
+
+// when working with realworld apps, the root entity level db objects are not exposed. to expose only the properties that are needed, DTOs are used.
+
+// first create the category table and push it to the db. to do that, create DbContext in the application. EF Core is used to create and manage the db object. 
