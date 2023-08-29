ff6f25179c1cc2c6f50cb16a540ac2d9646ec4a4
porslin <haashira@icloud.com>
2023-08-29 22:59:12 +0200

    making delete confirmation its own component

--- Corona_Server/wwwroot/js/common.js
+++ Corona_Server/wwwroot/js/common.js
@@ -27,3 +27,11 @@ window.ShowSwal = (type, message) => {
         )
     }
 }
+
+function ShowDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+}
